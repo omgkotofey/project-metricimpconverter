56cd6be76634bee013a7cefb673cfcaa2218a1a4
OMG Kotofey <omgkotofey@gmail.com>
2022-04-18 20:36:49 +0000

    Added implementation for convertHandler

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,42 +1,106 @@
+const UnitLib = {
+  gal: {
+    short: 'gal',
+    verbose: 'gallones',
+  },
+  km: {
+    short: 'km',
+    verbose: 'kilometres',
+  },
+  mi: {
+    short: 'mi',
+    verbose: 'miles',
+  },
+  lbs: {
+    short: 'lbs',
+    verbose: 'pounds',
+  },
+  L: {
+    short: 'L',
+    verbose: 'liters',
+  },
+}
+
 function ConvertHandler() {
+
+  const convertUnits = {
+    gal: {
+      ...UnitLib.gal,
+      to: UnitLib.L,
+      ratio: 3.78541,
+    },
+    mi: {
+      ...UnitLib.mi,
+      to: UnitLib.km,
+      ratio: 1.60934,
+    },
+    lbs: {
+      ...UnitLib.lbs,
+      to: UnitLib.kg,
+      ratio: 0.453592,
+    },
+    L: {
+      ...UnitLib.L,
+      to: UnitLib.gal,
+      ratio: 0.264172,
+    },
+    km: {
+      ...UnitLib.km,
+      to: UnitLib.mi,
+      ratio: 0.621371,
+    },
+    kg: {
+      ...UnitLib.kg,
+      to: UnitLib.lbs,
+      ratio: 2.204622,
+    }
+  }
+
+  const findConverConfig = (unitName) => {
+    if (!convertUnits.hasOwnProperty(unitName)) {
+      throw new Error('Invalid unit');
+    }
+
+    return convertUnits[unitName];
+  }
   
   this.getNum = function(input) {
-    let result;
+    // like '1' or '1.5' or '3/2'
+    let matches = input.match(/[\d\.\/]*/);
+    if (!matches) {
+      return 1;
+    }
+
+    const result = matches.shift();
+    // for '4//' or '4..' or '4/.'
+    if (result.match(/[\/\.]{1}/g).length > 1) {
+      throw new Error('Invalid number');
+    }
     
     return result;
   };
   
   this.getUnit = function(input) {
-    let result;
-    
-    return result;
+    // like 'km' or 'l'
+    let matches = input.match(/\D+$/);
+    if (!matches) {
+      throw new Error('Invalid unit');
+    }
+    return matches.shift();
   };
   
   this.getReturnUnit = function(initUnit) {
-    let result;
-    
-    return result;
+    return findConverConfig(initUnit).to.short;
   };
 
-  this.spellOutUnit = function(unit) {
-    let result;
-    
-    return result;
+  this.convert = function(initNum, initUnit) {    
+    return initNum * findConverConfig(initUnit).ratio;
   };
   
-  this.convert = function(initNum, initUnit) {
-    const galToL = 3.78541;
-    const lbsToKg = 0.453592;
-    const miToKm = 1.60934;
-    let result;
+  this.getString = function(initNum, initUnit, returnNum) {
+    const convertConfig = findConverConfig(initUnit);
     
-    return result;
-  };
-  
-  this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-    let result;
-    
-    return result;
+    return `${initNum} ${convertConfig.verbose} converts to ${returnNum} ${convertConfig.to.verbose}`;
   };
   
 }
